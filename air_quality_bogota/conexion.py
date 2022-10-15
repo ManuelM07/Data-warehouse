f0e101da8ebdbbd723e5103cda6996e06d4594fa
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2022-10-15 15:33:42 -0500

    Update conexion.py

--- air_quality_bogota/conexion.py
+++ air_quality_bogota/conexion.py
@@ -7,7 +7,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine(f'postgresql+psycopg2://postgres:pg123@127.0.0.1:5432/airQuality')
+#PASSWORD = os.environ["PG_PASSWORD"]
+
+dfx = pd.read_csv("air_quality_bogota/data/stations_loc.csv") # dim stations
+# engine = create_engine(f'postgresql+psycopg2://postgres:{PASSWORD}@localhost:5432/airQuality')
+engine = create_engine('postgresql+psycopg2://root:root@127.0.0.1:5432/airQuality')
 
 def new_model(df, name_model):
     df = df.rename_axis('id').reset_index()
@@ -23,3 +27,5 @@ def new_model(df, name_model):
     contents = output.getvalue()
     cur.copy_from(output, name_model, null="") # null values become ''
     conn.commit()
+
+#new_model(dfx, "testx")
